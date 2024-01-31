567920e3b3d0350840bd486577b14ab64bcac561
AthenasCode <emoore5203@gmail.com>
2024-01-31 23:00:57 +0800

    added and styled user image

--- quick-links/src/app/components/DashboardHeader.tsx
+++ quick-links/src/app/components/DashboardHeader.tsx
@@ -4,14 +4,21 @@ import Link from "next/link";
 import SignIn from "./SignIn";
 import SignOut from "./SignOut";
 import styles from "../css/header.module.css";
+import Image from "next/image";
 
 export default async function DashboardHeader() {
   const session = await getServerSession();
+
+  console.log(session);
 
   return (
     <header className={styles.dashboardHeader}>
       <h1>quicklinks</h1>
       <Navigation />
+      <button className={styles.userImg}>
+        <Image fill src={session?.user?.image ?? ""} alt="Profile image" />
+      </button>
+
       {session ? <SignOut /> : <SignIn />}
     </header>
   );
